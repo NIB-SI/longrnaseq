873421d14df2c718a87d676b9ef4561e09b710ae
nadjano <nnolte@ebi.ac.uk>
2025-11-12 14:02:09 +0100

    Update run.sh to include new input parameters and output directories for Atlantic and Rice Nipponbare samples
    - Changed output directory for Atlantic sample run from 'output_atlantic' to 'output_test'.
    - Updated fasta file path for Atlantic sample run.
    - Added new run for Rice Nipponbare samples with specific input parameters and output directory.
    - Included centrifuge database and SQANTI directory for Rice Nipponbare sample run.

--- run.sh
+++ run.sh
@@ -2,16 +2,24 @@
 
 conda activate nextflow
 
-nextflow run main.nf -profile singularity -with-report report.html -with-trace trace.txt -with-timeline timeline.html -with-dag flowchart.png -resume
-
 
 
 nextflow run main.nf  -profile singularity \
                     --input assets/samplesheet.csv \
-                    --outdir output_atlantic \
-                    --fasta /scratch/nadjafn/polyase_test/test_data/ATL_v3.asm.with_chloroplast_and_mito.fa \
+                    --outdir output_test \
+                    --fasta /scratch/nadjafn/LR_DESIREE_PAPER/zenodo/longreadrna_testrun/ \
                     --gtf  /scratch/nadjafn/polyase_test/test_data/unitato2Atl.with_chloroplast_and_mito.no_scaffold.agat.gtf \
-                    --technology ONT --downsample_rate 0.99  --skip_centrifuge --skip_sqanti -resume \
+                    --technology ONT --downsample_rate 0.99  --skip_centrifuge --skip_sqanti -resume --sqanti_dir /scratch/nadjafn/sqanti3/SQANTI3 \
                     #--centrifuge_db /biodbs/centrifuge/dbs_v2018/ \
-                    #--sqanti_dir /scratch/nadjafn/sqanti3/SQANTI3 \
-                   
+                     \
+        
+
+
+nextflow run main.nf -profile singularity \
+                    --input assets/samplesheet_rice_Nip_2samples.csv \
+                    --outdir output_rice_Nip_test \
+                    --fasta /scratch/nadjafn/LR_DESIREE_PAPER/ANALYSIS/rice_example/genome/Hap1_2_Nipponbare.renamed.organels.fasta \
+                    --gtf   /scratch/nadjafn/LR_DESIREE_PAPER/ANALYSIS/rice_example/genome/Hap1_2_Nipponbare.genome.renamed.organels.standard.gtf \
+                    --centrifuge_db /biodbs/centrifuge/dbs_v2018/ \
+                    --sqanti_dir /scratch/nadjafn/sqanti3/SQANTI3 \
+                    --technology ONT --downsample_rate 0.01 
